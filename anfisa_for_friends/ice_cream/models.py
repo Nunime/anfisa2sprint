50530d250f3532da492ceeef5e6a9ff9b49dd6c1
Gavrikov Nikita <nikitagav2004@gmail.com>
2025-03-05 14:52:23 +0300

    upd: ice_cream/admin, ice_cream.models

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -29,7 +29,10 @@ class Topping(PublishedModel):
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name = 'Название')
+    title = models.CharField('Название',
+        max_length = 256,
+        help_text = 'Уникальное название обёртки, не более 256 символов'
+    )
 
     class Meta:
         verbose_name = 'обёртка'
